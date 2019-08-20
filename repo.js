a8cdf87f852c5f4b5ce21039494e2d53d63649d2
rosenrose <crazytempler@gmail.com>
2019-08-20 10:47:46 +0900

    create repo

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "crazytempler@gmail.com",
+	"id": "rosenrose",
+	"patches": {
+		"ufo_vivid": "Makes the UFO more noticeable.\nby Dide, https://snipestalker.blog.me/221508374213"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/rosenrose/thmod/master/"
+	],
+	"title": "rosenrose's Patch Repo"
+}
---
+++ ufo_vivid/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "ufo_vivid",
+	"servers": [
+		"https://raw.githubusercontent.com/rosenrose/thmod/master/ufo_vivid/"
+	],
+	"title": "Makes the UFO more noticeable.\nby Dide, https://snipestalker.blog.me/221508374213"
+}
