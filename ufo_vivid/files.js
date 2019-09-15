1cefff9614ec967f197e856fcecd0835e41b81a1
rosenrose <crazytempler@gmail.com>
2019-09-16 04:34:34 +0900

    remove M

--- ufo_vivid/files.js
+++ ufo_vivid/files.js
@@ -1,6 +1,6 @@
 {
 	"patch.js": 1950621488,
 	"th12/bullet/etama8.png": 1208293634,
-	"th12/enemy/ufo_anim.png": 2613652824,
+	"th12/enemy/ufo_anim.png": 2946361494,
 	"th12/front/front00.png": 136556848
 }
