602d10ed45720386e0bbc405fb4293252bcac4ac
rosenrose <crazytempler@gmail.com>
2019-09-16 00:56:13 +0900

    upload th08 swap

--- repo.js
+++ repo.js
@@ -2,8 +2,8 @@
 	"contact": "crazytempler@gmail.com",
 	"id": "rosenrose",
 	"patches": {
+		"th08_swapping": "Changes the player pairs: (Reimu,Marisa),(Sakuya,Alice),(Youmu,Remilia),(Yukari,Yuyuko)",
 		"ufo_vivid": "Makes the UFO more noticeable.\n        by Dide, https://snipestalker.blog.me/221508374213"
-		"th08_swapping": "Changes the player pairs: (Reimu,Marisa),(Sakuya,Alice),(Youmu,Remilia),(Yukari,Yuyuko)"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/rosenrose/thmod/master/"
---
+++ th08_swapping/files.js
@@ -0,0 +1,19 @@
+{
+	"patch.js": 3582206461,
+	"th08/data/player00/player00.png": 103839481,
+	"th08/data/player00/player01.png": 4011624586,
+	"th08/data/player00/player02.png": 3794733702,
+	"th08/data/player00/player03.png": 1644310307,
+	"th08/data/title/sl_pl00a.png": 202846418,
+	"th08/data/title/sl_pl00h.png": 277957658,
+	"th08/data/title/sl_pl01a.png": 2632434981,
+	"th08/data/title/sl_pl01h.png": 660656523,
+	"th08/data/title/sl_pl02a.png": 4002667439,
+	"th08/data/title/sl_pl02h.png": 1600998602,
+	"th08/data/title/sl_pl03a.png": 446682776,
+	"th08/data/title/sl_pl03h.png": 2101545127,
+	"th08/ply00as.sht": 1330067077,
+	"th08/ply01a.sht": 2450607452,
+	"th08/ply02a.sht": 553770770,
+	"th08/ply03a.sht": 636163308
+}
