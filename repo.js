eaf48a3222fc5a72e9cedbbdcac1ea3cb978cf60
rosenrose <crazytempler@gmail.com>
2019-09-16 00:55:16 +0900

    upload th08 swap

--- repo.js
+++ repo.js
@@ -3,6 +3,7 @@
 	"id": "rosenrose",
 	"patches": {
 		"ufo_vivid": "Makes the UFO more noticeable.\n        by Dide, https://snipestalker.blog.me/221508374213"
+		"th08_swapping": "Changes the player pairs: (Reimu,Marisa),(Sakuya,Alice),(Youmu,Remilia),(Yukari,Yuyuko)"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/rosenrose/thmod/master/"
---
+++ th08_swapping/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "th08_swapping",
+	"servers": [
+		"https://raw.githubusercontent.com/rosenrose/thmod/master/th08_swapping/"
+	],
+	"title": "Changes the player pairs: (Reimu,Marisa),(Sakuya,Alice),(Youmu,Remilia),(Yukari,Yuyuko)"
+}
