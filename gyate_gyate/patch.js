f3ed9ac3d6de0bd7c479e2872b033fbfb8c142f2
rosenrose <crazytempler@gmail.com>
2020-01-21 16:51:25 +0900

    add gyate_th06

---
+++ gyate_gyate/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "gyate_gyate",
+	"servers": [
+		"https://raw.githubusercontent.com/rosenrose/thmod/master/gyate_gyate/"
+	],
+	"title": "Gyate_gyate faces."
+}
--- repo.js
+++ repo.js
@@ -7,6 +7,7 @@
 		"swappedShots": "(TH07)ReimuA→MarisaA style, ReimuB→SakuyaB style, MarisaA→ReimuA style, MarisaB→SakuyaB style\n        (TH10)MarisaA→ReimuC style, MarisaC→ReimuA style\n        (TH11)ReimuA→MarisaA style, ReimuB→MarisaB style, MarisaB→ReimuB style",
 		"th08_swapping": "Changes the player pairs: (Reimu,Marisa),(Sakuya,Alice),(Youmu,Remilia),(Yukari,Yuyuko)",
 		"ufo_vivid": "UFO patch for the color blind people.\n        by Dide, https://snipestalker.blog.me/221508374213"
+		"gyate_gyate": "Gyate_gyate faces."
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/rosenrose/thmod/master/"
