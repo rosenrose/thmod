2dd220ccda0fdadf577072af784453ecef5cea99
rosenrose <crazytempler@gmail.com>
2020-01-21 16:58:39 +0900

    add filelist

--- repo.js
+++ repo.js
@@ -4,10 +4,10 @@
 	"patches": {
 		"8options": "(TH11)Reimu's all types and MarisaC can have 8 options.",
 		"bottom_enemy": "Indicates \"enemy\" position like pre-th11 style.",
+		"gyate_gyate": "Gyate_gyate faces.",
 		"swappedShots": "(TH07)ReimuA→MarisaA style, ReimuB→SakuyaB style, MarisaA→ReimuA style, MarisaB→SakuyaB style\n        (TH10)MarisaA→ReimuC style, MarisaC→ReimuA style\n        (TH11)ReimuA→MarisaA style, ReimuB→MarisaB style, MarisaB→ReimuB style",
 		"th08_swapping": "Changes the player pairs: (Reimu,Marisa),(Sakuya,Alice),(Youmu,Remilia),(Yukari,Yuyuko)",
 		"ufo_vivid": "UFO patch for the color blind people.\n        by Dide, https://snipestalker.blog.me/221508374213"
-		"gyate_gyate": "Gyate_gyate faces."
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/rosenrose/thmod/master/"
