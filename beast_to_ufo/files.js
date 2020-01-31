2ac6ac6c2b96856fc9b354a8e7f8c48debb53cf2
rosenrose <crazytempler@gmail.com>
2020-02-01 04:12:30 +0900

    add beastot

--- beast_to_ufo/files.js
+++ beast_to_ufo/files.js
@@ -1,5 +1,6 @@
 {
 	"patch.js": 3256356024,
 	"th17/bullet/beast1.png": 2400364076,
-	"th17/bullet/beastf.png": 1987995132
+	"th17/bullet/beastf.png": 1987995132,
+	"th17/bullet/beastot.png": 3939098671
 }
