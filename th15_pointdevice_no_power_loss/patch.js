e6c6347b772d87a4ec84172d582a78eec542a0b9
rosenrose <crazytempler@gmail.com>
2020-02-04 16:51:34 +0900

    edit powerloss

--- th15_pointdevice_no_power_loss/patch.js
+++ th15_pointdevice_no_power_loss/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://raw.githubusercontent.com/rosenrose/thmod/master/th15_pointdevice_no_power_loss/"
 	],
-	"title": "Press C to teleport.(made by 335 from MUASTG)"
+	"title": "No power loss in pointdeivce mode.(made by 32th-System)"
 }
--- th15_pointdevice_no_power_loss/th15.js
+++ th15_pointdevice_no_power_loss/th15.js
@@ -1,9 +1,9 @@
 {
 	"binhacks": {
 		"pointdevice_no_power_loss_hack": {
-			"addr": ["Rx43DF43","Rx43DF44","Rx43DF45"],
-			"code": "90"
+			"addr": "0x0043DF43",
+			"code": "90 90 90"
 		}
 	},
-	"title": "th165_C-key"
+	"title": "th15_pointdevice_no_power_loss"
 }
