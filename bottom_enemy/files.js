be8f2cfa9433517eaeeec262db1874ef15ec016e
rosenrose <42435748+rosenrose@users.noreply.github.com>
2021-05-19 16:39:46 +0900

    bottom enemy: add TH18

--- bottom_enemy/files.js
+++ bottom_enemy/files.js
@@ -6,5 +6,6 @@
 	"th143/front/front00.png": 407983597,
 	"th15/front/front00.png": 1634329882,
 	"th16/front/front00.png": 1203378190,
-	"th17/front/front00.png": 3350018650
+	"th17/front/front00.png": 3350018650,
+	"th18/front/front00.png": 3350018650
 }
